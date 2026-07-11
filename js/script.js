e58011727f9d06e875dbf400b5c9b083b19dd7f6
Ramprasad Karak <ramprasadkarak321@gmail.com>
2026-07-11 22:04:27 +0530

    create new section

--- js/script.js
+++ js/script.js
@@ -16,3 +16,35 @@ menuBtn.addEventListener("click", () => {
     open = true;
   }
 });
+
+// About Swiper slider
+var swiper = new Swiper(".prgrm-swiper", {
+  slidesPerView: 3,
+  spaceBetween: 30,
+  loop: true,
+  autoplay: {
+    delay: 3000,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  breakpoints: {
+    320: {
+      slidesPerView: 1,
+      // spaceBetween: 30,
+    },
+    600: {
+      slidesPerView: 2,
+      spaceBetween: 10,
+    },
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 30,
+    },
+    992: {
+      slidesPerView: 3,
+      spaceBetween: 30,
+    },
+  },
+});
