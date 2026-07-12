dcfcd222db0d1861b419751194b28c089090ecae
Ramprasad Karak <ramprasadkarak321@gmail.com>
2026-07-12 22:15:04 +0530

    update media

--- js/script.js
+++ js/script.js
@@ -18,12 +18,16 @@ menuBtn.addEventListener("click", () => {
 });
 
 // About Swiper slider
-var swiper = new Swiper(".prgrm-swiper", {
+let swiper = new Swiper(".prgrm-swiper", {
   slidesPerView: 3,
   spaceBetween: 30,
   loop: true,
   autoplay: {
     delay: 3000,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
   },
   pagination: {
     el: ".swiper-pagination",
@@ -32,7 +36,7 @@ var swiper = new Swiper(".prgrm-swiper", {
   breakpoints: {
     320: {
       slidesPerView: 1,
-      // spaceBetween: 30,
+      // spaceBetween: 50,
     },
     600: {
       slidesPerView: 2,
@@ -40,7 +44,7 @@ var swiper = new Swiper(".prgrm-swiper", {
     },
     768: {
       slidesPerView: 2,
-      spaceBetween: 30,
+      spaceBetween: 40,
     },
     992: {
       slidesPerView: 3,
